84ebac9a1096b7ddc6e3f2a22c735cb4b4ff0468
Jay Lee <slee9111@conestogac.on.ca>
2022-06-29 01:32:26 -0400

    Fix empty cart issue.

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-window.onload = (event) => {
-	console.log('page is fully loaded');
+const addToCart = document.querySelectorAll('.add-to-cart');
+const numOfItem = document.querySelector('.item-number');
+
+let itemsInCart = [];
+
+// ** function reload localStorage
+const reloadProducts = () => {
+	let products = localStorage.getItem('cart');
+	products = JSON.parse(products);
+	if (products) {
+		console.log(products.length);
+		numOfItem.textContent = products.length;
+		return products;
+	}
 };
+
+if (reloadProducts()) {
+	itemsInCart = reloadProducts();
+}
+
+// Take out $ and convert to int
+const parsePrice = (priceWithDollarSign) => {
+	return Number(priceWithDollarSign.substring(2));
+};
+
+// Create object of item info
+const parseItemInfo = (node) => {
+	return {
+		name: node[1].innerText,
+		price: parsePrice(node[2].innerText),
+	};
+};
+
+// Loop an array of addToCart buttons
+for (const item of addToCart) {
+	item.addEventListener('click', (e) => {
+		// rest operator
+
+		// Get object of the item with name and price
+		const itemInfo = parseItemInfo(item.parentNode.children);
+
+		// Save in localStorage
+		itemsInCart.push(itemInfo);
+		localStorage.setItem('cart', JSON.stringify(itemsInCart));
+
+		// // update quantity of items in the cart
+		numOfItem.textContent = itemsInCart.length;
+	});
+}
